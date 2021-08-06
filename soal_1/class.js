6065a98ee5468a8f0134831b2ef39339dc72c13c
erwinbaee <erwinsajaya131002@gmail.com>
2021-08-06 07:21:15 +0700

    soal_class

---
+++ soal_1/class.js
@@ -0,0 +1,35 @@
+class Animal {
+    constructor(name = '', age = 0, isMammal = true) {
+        this.name = name;
+        this.age = age;
+        this.isMammal = isMammal;
+    } 
+}
+
+class Rabbit extends Animal{
+    constructor(name, age, isMammal){
+        super(name, age, isMammal = true)
+        this.isMammal = isMammal
+        this.age = age
+    }
+    eat() {
+        return `${this.name} sedang makan!`
+    }
+    
+}
+
+class Eagle extends Animal{
+    constructor(name, age, isMammal){
+        super(name, age, isMammal = false)
+        this.isMammal = isMammal
+        this.age = age
+    }
+    fly(){
+        return `${this.name} sedang terbang!`
+    }
+}
+let myRabbit = new Rabbit(`Labi`,2);
+console.log( myRabbit.eat());
+
+let myEagle = new Eagle(`Elo`,4);
+console.log(myEagle.fly());
